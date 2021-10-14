0c758d0ac015d2adf09bcae81123d8fa942fbbb2
alex <mmerenstein@cs.stonybrook.edu>
2021-10-14 08:23:03 -0400

    Dont umont/mount cache directory since its used by docker now

--- f3/experiments/disk-speed-tests/dd-test/do-read-test.sh
+++ f3/experiments/disk-speed-tests/dd-test/do-read-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-umount /mnt/local-cache
+#umount /mnt/local-cache
 echo 1 > /proc/sys/vm/drop_caches
-mount /dev/sda4 /mnt/local-cache
+#mount /dev/sda4 /mnt/local-cache
 /usr/bin/time -f "TOTAL,$1,%e,`date +%s`" bash -c "dd if=/mnt/local-cache/f of=/dev/null bs=4M count=$2; sync"
