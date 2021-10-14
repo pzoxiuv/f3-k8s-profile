3005467552b23382b319502545dd4a7dec6d07be
alex <mmerenstein@cs.stonybrook.edu>
2021-10-14 08:16:16 -0400

    Call sync after reading/writing

--- f3/experiments/micro-benchmark/run-nostats-f3only.sh
+++ f3/experiments/micro-benchmark/run-nostats-f3only.sh
@@ -10,7 +10,8 @@ echo "date,`date +%s`"
 kubectl exec f3-testing1-pod-kubes1 -nopenwhisk -- /vmtouch -q -e $1
 
 before1=$(date +%s)
-kubectl exec f3-testing1-pod-kubes1 -nopenwhisk -- /writer $1 $FILESIZE
+#kubectl exec f3-testing1-pod-kubes1 -nopenwhisk -- /writer $1 $FILESIZE
+kubectl exec f3-testing1-pod-kubes1 -nopenwhisk -- bash -c "/writer $1 $FILESIZE; sync"
 after1=$(date +%s)
 write_time=$(( $after1 - $before1 ))
 #echo "$(( $after1 - $before1 ))"
@@ -21,13 +22,14 @@ kubectl exec f3-testing1-pod-kubes3 -nopenwhisk -- stat $1 >/dev/null
 kubectl exec f3-testing1-pod-kubes3 -nopenwhisk -- ls -lh /var/f3 >/dev/null
 
 before2=$(date +%s)
-kubectl exec f3-testing1-pod-kubes3 -nopenwhisk -- /reader $1
+#kubectl exec f3-testing1-pod-kubes3 -nopenwhisk -- /reader $1
+kubectl exec f3-testing1-pod-kubes3 -nopenwhisk -- bash -c "/reader $1; sync"
 after2=$(date +%s)
 read_time=$(( $after2 - $before2 ))
 #echo $before2 $after2
 #echo "read,$(( $after2 - $before2 ))"
 #echo "$(( $write_time + $read_time )) s total $1 YYY"
-echo "$(( $after1 - $before1 )),$(( $after2 - $before2 )),$(( $write_time + $read_time )),$3,YYY"
+echo "$(( $after1 - $before1 )),$(( $after2 - $before2 )),$(( $write_time + $read_time )),$(( $after2 - $before1 )),$3,YYY"
 
 kubectl exec f3-testing1-pod-kubes1 -nopenwhisk -- rm $1
 sudo -u amerenst ssh node-1 sudo rm -rf /mnt/local-cache/tempdir/*
